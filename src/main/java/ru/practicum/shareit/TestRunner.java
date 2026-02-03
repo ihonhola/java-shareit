4c352a801dc63d4b76d6c7bd8e5ae09591dff3a0
Anatoly Yakovlev <ihonhola@yandex.ru>
2026-02-03 10:53:06 +0300

    ФЗ спринта 15 fix 2: исправление замечаний ревьювера

--- src/main/java/ru/practicum/shareit/TestRunner.java
+++ src/main/java/ru/practicum/shareit/TestRunner.java
@@ -3,6 +3,7 @@ package ru.practicum.shareit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Deprecated
 @SpringBootApplication
 public class TestRunner {
 
